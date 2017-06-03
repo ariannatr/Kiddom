3c824e098c96eb4b56d86f52385f66d10f3fbb36
ariannatr <sdi1300179@di.uoa.gr>
2017-06-04 02:23:55 +0300

    trying to make infos visible

---
+++ src/main/java/kiddom/repository/ActivityRepository.java
@@ -0,0 +1,12 @@
+package kiddom.repository;
+
+import kiddom.model.SingleEventEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by Arianna on 3/6/2017.
+ */
+@Repository("activityRepository")
+public interface ActivityRepository extends JpaRepository<SingleEventEntity, Long> {
+}
