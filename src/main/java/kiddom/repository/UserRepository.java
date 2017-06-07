553ce8fc07dc9a19d99c30b1f38ab01e785ca682
DimChris0 <dim.chris92@gmail.com>
2017-06-07 14:18:37 +0300

    google_map getting and showing address, need more work -> array for getting all possible addresses

--- src/main/java/kiddom/repository/UserRepository.java
+++ src/main/java/kiddom/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 import kiddom.model.UserEntity;
 
 @Repository("userRepository")
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, String> {
 	 UserEntity findByUsername(String username);
 
 //	void saveUser(UserEntity user, ParentEntity parent);
